58cdc8ee2db394b2e1b374b27727141609e6b49d
Sasha <aleksandrpaprika@gmail.com>
2024-01-22 13:44:15 +0400

    Have been added locator and method in HomePage

--- src/test/java/applePages/HomePage.java
+++ src/test/java/applePages/HomePage.java
@@ -7,13 +7,21 @@ public class HomePage extends BasePage {
 
     public final By searchButton = By.xpath("//li[@data-topnav-flyout-label='Search apple.com']");
     public final By searchField = By.xpath("//input[@class='globalnav-searchfield-input']");
+    public final By searchProductButton = By.xpath("//button[@class='globalnav-searchfield-submit']");
 
 
     public void clickSearchButton() {
         driver.findElement(searchButton).click();
     }
 
-    public void writeProduct() {
-        driver.findElement(searchField).sendKeys("iPhone 15");
+    public void writeNameOfProduct(String productName) {
+        driver.findElement(searchField).sendKeys(productName);
+
     }
+
+    public void clickSearchProduct() {
+        driver.findElement(searchProductButton).click();
+    }
+
+
 }
