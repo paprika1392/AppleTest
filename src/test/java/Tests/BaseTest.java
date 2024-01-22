cd82625daa723099b29aada29f07f2b4955f4b48
Sasha <aleksandrpaprika@gmail.com>
2024-01-22 13:49:43 +0400

    Have been added annotations before and after in BaseTest

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -1,8 +1,21 @@
 package Tests;
 
-public class BaseTest  {
+import Steps.Steps;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
+public class BaseTest {
 
+    @BeforeTest
+    public static void startWorkDriver() {
+        Steps.openBrowser();
+        Steps.clearCookies();
+    }
+
+    @AfterTest
+    public static void finishWorkDriver() {
+        Steps.closeBrowser();
+    }
 
 
 }
