f95e6e8b3dd775bb5b1abfc01415492ebe5e25af
Sasha <aleksandrpaprika@gmail.com>
2024-01-22 13:43:37 +0400

    Have been added steps methods

--- src/test/java/Steps/Steps.java
+++ src/test/java/Steps/Steps.java
@@ -1,4 +1,33 @@
 package Steps;
 
+import Utils.Browser;
+import applePages.HomePage;
+import org.testng.annotations.Test;
+
+import static Utils.Constants.Constants.APPLE_HOME_PAGE_URL;
+
 public class Steps {
+
+    public static HomePage homePage = new HomePage();
+
+    public static void openBrowser() {
+        Browser.webDriverStartWork();
+        Browser.setUrl(APPLE_HOME_PAGE_URL);
+        Browser.customizeBrowserWindow();
+    }
+
+    public static void clearCookies() {
+        Browser.clearCookiesAndLocalStorage();
+    }
+
+    public static void enterTheDeviseName(String productName) {
+        homePage.clickSearchButton();
+        homePage.writeNameOfProduct(productName);
+        homePage.clickSearchProduct();
+
+    }
+
+    public static void closeBrowser() {
+        Browser.closeBrowser();
+    }
 }
