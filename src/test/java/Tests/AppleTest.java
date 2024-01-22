d1a36e2c5066df11a8c66b0f46bd7920264c7fe2
Sasha <aleksandrpaprika@gmail.com>
2024-01-22 15:55:07 +0400

    Have been created AppleTest class with methods and @Test

--- src/test/java/Tests/AppleTest.java
+++ src/test/java/Tests/AppleTest.java
@@ -1,5 +1,13 @@
 package Tests;
 
-public class AppleTest {
+import Steps.Steps;
+import org.testng.annotations.Test;
 
+public class AppleTest extends BaseTest {
+
+    @Test
+    public void test() {
+        Steps.enterProductName("iPhone 15");
+        Steps.FindAndEqualsProducts("iPhone 15");
+    }
 }
