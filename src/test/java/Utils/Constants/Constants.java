08e8cc8e1dab5c3d6c71e9484e5b87eeb935956c
Sasha <aleksandrpaprika@gmail.com>
2024-01-30 15:40:32 +0400

    Have been created new constant INCORRECT_ID_OR_NAME_ERROR

--- src/test/java/Utils/Constants/Constants.java
+++ src/test/java/Utils/Constants/Constants.java
@@ -3,4 +3,5 @@ package Utils.Constants;
 public class Constants {
     public static final String APPLE_HOME_PAGE_URL ="https://www.apple.com/";
     public static final String APPLE_SEARCHING_PRODUCT_NAME = "iPhone 15";
+    public static final String INCORRECT_ID_OR_NAME_ERROR = "Your Apple ID or password was incorrect";
 }
